6dc9b384acb104218a50435b8198dcefaec42be5
Andrew Thielking <athielking@gmail.com>
2020-12-28 13:40:55 -0500

    added controller for testing indentities

---
+++ SpaApi/SpaApi/Controllers/UserController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using Microsoft.Identity.Client;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace SpaApi.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            string[] authValues = null;
+            if( HttpContext.Request.Headers.TryGetValue("Authorization", out var authValue) )
+            {
+                authValues = authValue.ToArray();
+            }
+
+            return Ok(new
+            {
+                userName = User.Identity.Name,
+                authHeader = authValues
+            });
+        }
+    }
+}
